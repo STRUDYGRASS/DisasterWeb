f38aee520c869015d7ec513f61c56a67f4b330b4
fanshuaidi <1443183646@qq.com>
2020-02-15 02:30:23 +0800

    bug修复

--- php_func/process_func.php
+++ php_func/process_func.php
@@ -1,11 +1,10 @@
-
 <?php
 
 	function mongo_to_json_hsp($data)
 	{
 		$data_need = [];
 		foreach ($data as $dataValue) {
-			$data_need[] = [$dataValue['坐标x'],$dataValue['坐标y'],$dataValue['医院']];
+			$data_need[] = [$dataValue['x'],$dataValue['y'],$dataValue['yiyuan']];
 			}
 		return json_encode($data_need);
 	}
@@ -14,7 +13,7 @@
 	{
 		$data_need = [];
 		foreach ($data as $dataValue) {
-			$data_need[] = [$dataValue['x'],$dataValue['y'],$dataValue['省'].$dataValue['市'].$dataValue['区（县）'].$dataValue['小区']];
+			$data_need[] = [$dataValue['x'],$dataValue['y'],$dataValue['xiaoqu']];
 			}
 		return json_encode($data_need);
 	}
