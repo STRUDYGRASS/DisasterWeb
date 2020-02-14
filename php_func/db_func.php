87caa45c208738ab0473f0d5cb67ac17028433ed
fanshuaidi <1443183646@qq.com>
2020-02-14 19:56:15 +0800

    test

--- php_func/db_func.php
+++ php_func/db_func.php
@@ -7,7 +7,7 @@
 		private function db_createManger()
 		{
 			try{
-				$mongodbUrl = "mongodb://10.0.0.9:27017";
+				$mongodbUrl = "mongodb://47.95.10.60:27017";
 				$username = "yiqing";
 				$password = "HQU@2017";
 			  	$connStr = $mongodbUrl;
@@ -15,7 +15,8 @@
 				   'username' => $username,
 				   'password' => $password,
 				  );
-			  	return new MongoDB\Driver\Manager($connStr, $options);
+			  	return new MongoDB\Driver\Manager($connStr);
+			  	//, $options
 			}
 			catch(Exception $e){
 	  			return false;
@@ -24,7 +25,7 @@
 
 		public function db_find($filter, $collection)
 		{
-			$mongoDB = "yiqing";
+			$mongoDB = "yiqingditu";
 			 $conn = $this->db_createManger();
 			 if (empty($conn)) 
 			 {
