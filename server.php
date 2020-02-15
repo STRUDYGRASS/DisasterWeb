5f9a06ec132a547d87f3b7de0428a3774aa4c204
root <root@localhost.localdomain>
2020-02-15 22:11:01 +0800

    服务器版本

--- server.php
+++ server.php
@@ -1,12 +1,12 @@
 <?php
-	include_once $_SERVER['DOCUMENT_ROOT']."/DisasterWeb/php_func/db_func.php";
-  	include_once $_SERVER['DOCUMENT_ROOT']."/DisasterWeb/php_func/process_func.php";
+	include_once $_SERVER['DOCUMENT_ROOT']."/php_func/db_func.php";
+  	include_once $_SERVER['DOCUMENT_ROOT']."/php_func/process_func.php";
 
 	$time = $_POST['time'];
 	$thing = $_POST['thing'];
 
 	$dbClass = new dbClass;
-	if($thing == "医院")
+	if($thing == "定点医院")
 	{
 		$filter = [];
 		$collection = 'yiyuan';
@@ -14,7 +14,7 @@
 		$data = mongo_to_json_hsp($data_array);
 		echo $data;
 	}
-	elseif ($thing == "小区") 
+	elseif ($thing == "详细地址") 
 	{
 		$filter = [];
 		$collection = 'dili';
