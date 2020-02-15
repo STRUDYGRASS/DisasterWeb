f1c4ad1b658eb9917fbbc8ecdb09e6f6911f2b45
fanshuaidi <1443183646@qq.com>
2020-02-15 12:33:24 +0800

    aks全部可用

--- php_func/rand_aks.php
+++ php_func/rand_aks.php
@@ -1,7 +1,7 @@
 <?php
 	function rand_aks()
 	{
-		$aks = array("Ck5MwFdvZW3R0OqG27fqNxbDPeodRUB6","i2tM7wogeLuw7LZtFladQIxCWkL3fUjD","SH2NPGNgDG87p6x41AZv3lOKDGSkKCCK","PmK5DyiprlSsD0pg1i3PGT9qL7EM4SsG","fiiQ7Nr8q3Yvamm1slbGWkCY70UykjQ8","WUIZXgr4TbPCgCA7INTnu6GADdpE6vkU","XXG5CO0mpTX9jKLCxYgtDNuBeK53hGDu","A9fKbmdvtbTbTypS5o3CStCsTDKu5GLQ","3ccb5362c4712a1dd2df47f58f49188a");
+		$aks = array("Ck5MwFdvZW3R0OqG27fqNxbDPeodRUB6","2z3OR5c4NNdb4sTbI4u1dbNSHRK929ha","lHeMiBL8TM4WcyRprCXsjOfafE4G0zi6","PmK5DyiprlSsD0pg1i3PGT9qL7EM4SsG","fHBrTsM7tcClb8VNBRPbGVmtsTHzLNn0","XIwxInWpkBxao8gutBoPZZTKXRNW4jEL","XXG5CO0mpTX9jKLCxYgtDNuBeK53hGDu","A9fKbmdvtbTbTypS5o3CStCsTDKu5GLQ","3ccb5362c4712a1dd2df47f58f49188a","3b2MXU9AQUm2uCdwk6NNagIS17vNjZ8K");
 		$site = array_rand($aks);
 		return $aks[$site];
 	}
