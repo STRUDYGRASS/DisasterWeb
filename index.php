62596250161685f3b3bc25ae4fd91c6ad845c094
fanshuaidi <1443183646@qq.com>
2020-02-16 11:16:50 +0800

    新增免责声明

--- index.php
+++ index.php
@@ -70,8 +70,34 @@
         <div id="allmap"></div>
         <div style="position: absolute; bottom: 5px; right: 45px;" class="div-clear">
             <span>
-                <button type="button" class="btn btn-danger" onclick="window.location.href = 'https://www.http://118.89.57.44/announciation.html'">
-                 <span class="glyphicon glyphicon glyphicon-zoom-in"></span> 免责声明
+                <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+<!-- 模态框（Modal） -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" 
+                        aria-hidden="true">×
+                </button>
+                <h4 class="modal-title" id="myModalLabel">
+                    模态框（Modal）标题
+                </h4>
+            </div>
+            <div class="modal-body">
+                按下 ESC 按钮退出。
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" 
+                        data-dismiss="modal">关闭
+                </button>
+                <button type="button" class="btn btn-primary">
+                    提交更改
+                </button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+                 <span class="glyphicon glyphicon glyphicon-zoom-in"></span>免责声明 
                 </button>
             </span>
         </div>
